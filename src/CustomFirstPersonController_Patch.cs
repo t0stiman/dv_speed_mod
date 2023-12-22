65493cb2495be33affb98ee6743e0a3a724a55a1
t0stiman <18124323+t0stiman@users.noreply.github.com>
2023-12-22 16:07:37 +0100

    initial commit

---
+++ src/CustomFirstPersonController_Patch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace speeeed
+{
+	[HarmonyPatch(typeof(CustomFirstPersonController))]
+	[HarmonyPatch("ProcessInputAndGetSpeed")]
+	public class CustomFirstPersonController_Patch
+	{
+		private static void Postfix(ref float __result)
+		{
+			__result *= Main.MySettings.movementSpeedMultiplier;
+		}
+	}
+	
+	[HarmonyPatch(typeof(CustomFirstPersonController))]
+	[HarmonyPatch("SetJumpParameters")]
+	public class SetJumpParameters_Patch
+	{
+		private static void Postfix(ref CustomFirstPersonController __instance)
+		{
+			__instance.m_MoveDir.y *= Main.MySettings.jumpSpeedMultiplier;
+		}
+	}
+}
---
+++ src/Settings.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+
+namespace speeeed
+{
+	public class Settings : UnityModManager.ModSettings
+	{
+		public float movementSpeedMultiplier = 2.5f;
+		private string moveText = "";
+		
+		public float jumpSpeedMultiplier = 2f;
+		private string jumpText = "";
+		
+		public void Draw(UnityModManager.ModEntry modEntry)
+		{
+			{
+				if (moveText == "")
+				{
+					moveText = movementSpeedMultiplier.ToString();
+				}
+
+				GUILayout.Label("Movement speed multiplier");
+				moveText = GUILayout.TextField(moveText);
+
+				if (float.TryParse(moveText, out var newValue))
+				{
+					movementSpeedMultiplier = newValue;
+				}
+				else
+				{
+					GUILayout.Label($"'{moveText}' is not a number");
+				}
+			}
+			
+			// ====================
+			
+			{
+				if (jumpText == "")
+				{
+					jumpText = jumpSpeedMultiplier.ToString();
+				}
+
+				GUILayout.Label("Jump speed multiplier");
+				jumpText = GUILayout.TextField(jumpText);
+
+				if (float.TryParse(jumpText, out var newValue))
+				{
+					jumpSpeedMultiplier = newValue;
+				}
+				else
+				{
+					GUILayout.Label($"'{jumpText}' is not a number");
+				}
+			}
+		}
+
+		public override void Save(UnityModManager.ModEntry modEntry)
+		{
+			Save(this, modEntry);
+		}
+	}
+}
